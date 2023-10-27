930430073607d4daf54bea57be9e6cdc8bf45ac8
Usuario <jstorres103050@umanizales.edu.co>
2023-10-27 00:09:09 -0500

    me mori f

---
+++ src/main/java/com/tads_prog2/controller/dto/CityStructureDEDTO.java
@@ -0,0 +1,11 @@
+package com.tads_prog2.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CityStructureDEDTO {
+    private String city;
+    private int quantity;
+}
---
+++ src/main/java/com/tads_prog2/controller/dto/GenderStructureDTO.java
@@ -0,0 +1,11 @@
+package com.tads_prog2.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class GenderStructureDTO {
+    private String gender;
+    private int quantity;
+}
